477b05d93628d60e7826e75d647e638a56a38220
kvraghava <50203323+kvraghava@users.noreply.github.com>
2023-04-08 17:35:21 +0530

    v2 version

---
+++ limit-service/src/main/java/com/raghava/service/controller/LimitsController.java
@@ -0,0 +1,22 @@
+package com.raghava.service.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.raghava.service.configuration.Configuration;
+import com.raghava.service.model.Limits;
+
+@RestController
+public class LimitsController {
+
+	@Autowired
+	private Configuration configuration;
+
+	@GetMapping("/limits")
+	public Limits retrieveLimits() {
+		return new Limits(configuration.getMinimum(), 
+				configuration.getMaximum());
+//		return new Limits(1,1000);
+	}
+}
